28d085956d9f2e2c28ea22eb2710a64b83bc2fab
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2025-06-08 14:10:13 -0400

    first chara

---
+++ Assets/Character/Character.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct CharacterState
+{
+    [field: SerializeField] String StateName {get;set;}
+    [field: SerializeField] Sprite StateSprite {get;set;}
+}
+public class Character : MonoBehaviour
+{
+    [field: SerializeField] int MealCount { get; set; }
+    [field: SerializeField] int DayCount { get; set; }
+    [field: SerializeField] List<CharacterState> CharacterStates {get;set;}
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
